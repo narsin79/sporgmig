65b9f4334f4d6c9c0a2bf23f528af9797ee96dc2
narinder <ns@a-vask.dk>
2018-07-21 12:50:25 +0200

    change again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 test</div>
+                <div class="title">Laravel 5 test again</div>
             </div>
         </div>
     </body>
